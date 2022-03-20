275762472133fa56743764cd570b513a35abe98e
Danny Shen <shenfive@gmail.com>
2022-03-20 16:49:49 +0800

    Initial Commit

---
+++ a202SwiftUI01/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  a202SwiftUI01
+//
+//  Created by 申潤五 on 2022/3/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ a202SwiftUI01/a202SwiftUI01App.swift
@@ -0,0 +1,17 @@
+//
+//  a202SwiftUI01App.swift
+//  a202SwiftUI01
+//
+//  Created by 申潤五 on 2022/3/20.
+//
+
+import SwiftUI
+
+@main
+struct a202SwiftUI01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
