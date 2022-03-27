d1897526f38c927276c37e6eebcfceb934a56408
Danny Shen <shenfive@gmail.com>
2022-03-27 13:03:39 +0800

    set var

--- a202SwiftUI01/ContentView.swift
+++ a202SwiftUI01/ContentView.swift
@@ -8,11 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var r:Double = 0.5
+    @State var g:Double = 0.5
+    @State var b:Double = 0.5
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Color(red: r, green: g, blue: b)
+            ctrl
+        }
+    }
+    
+    var ctrl:some View{
+        VStack{
+            HStack{
+                Text("R:\(Int(r*255))").font(.largeTitle)
+                Text("G:\(Int(g*255))").font(.largeTitle)
+                Text("B:\(Int(b*255))").font(.largeTitle)
+            }
+            Spacer()
+            VStack{
+                Slider(value: $r)
+                Slider(value: $g)
+                Slider(value: $b)
+            }.padding(.all,30)
+        }
     }
 }
+    
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
